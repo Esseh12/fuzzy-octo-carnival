3d805f5bc341580eb5a7f1f8f0deb4420874064e
Esseh12 <Gbenmoese788@gmail.com>
2023-06-26 14:53:32 +0000

    initials.c

---
+++ bigdev.c/initials.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("my initials are %c , %c , %c and my age is %d\n", 'G', 'E','D', 20);
+	return (0);
+}
