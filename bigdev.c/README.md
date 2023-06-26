d1f899a244d7f50b52846ac34da8534268af575b
Esseh12 <Gbenmoese788@gmail.com>
2023-06-26 09:14:25 +0000

    Developer-tool.c

---
+++ bigdev.c/README.md
@@ -0,0 +1,1 @@
+inside this directory contains all my c programming code , i dont know what i am doing but ill be the best at it
---
+++ bigdev.c/developer-tool.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char Name[] = "Ese";
+	int Age = 20;
+
+	printf("I have a beautiful friend named %s\n", Name);
+	printf("she had skin like caramel\n");
+	printf("her skills made it hard to tell that she was %d\n", Age);
+	return (0);
+}
