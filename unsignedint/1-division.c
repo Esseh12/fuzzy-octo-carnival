7852e779fc991e811c43ef9f4694f71b69ce99c5
Esseh12 <Gbenmoese788@gmail.com>
2023-07-06 09:40:06 +0000

    division

---
+++ unsignedint/1-division.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - random division
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	unsigned int num = 200;
+	unsigned int num_2 = 10;
+	unsigned int result = num / num_2;
+
+	printf("Result:%d\n", result);
+	return (0);
+}
