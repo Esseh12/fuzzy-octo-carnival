98d0fb70be0e719de1b2ed03de3a5c74383ddfca
Esseh12 <Gbenmoese788@gmail.com>
2023-07-06 14:14:54 +0000

    intenseadd

---
+++ unsignedint/100-intenseadd.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Intense addition randomly
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	unsigned int num = 200;
+	int max = -100;
+	unsigned int result = num + max;
+
+	printf("Result:%d\n", result);
+
+	return (0);
+}
+	
