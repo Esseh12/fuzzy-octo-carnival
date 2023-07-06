27c52b585a2e898ebf5be3a6ca4dfd1e6dff1401
Esseh12 <Gbenmoese788@gmail.com>
2023-07-06 09:13:22 +0000

    0-addition

---
+++ unsignedint/0-addition.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - addition of random numbers
+ *
+ * Return: Answer 0 (success)
+ */
+int main(void)
+{
+	unsigned int num = 500;
+	unsigned int maximum = 300;
+	unsigned int result = num + maximum;
+
+	printf("Result:%d\n", result);
+	
+	return (0);
+}
---
+++ unsignedint/README.md
@@ -0,0 +1,1 @@
+Random unsigned int
