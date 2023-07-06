a534491a0ead161293f8502411db4b2f759f4412
Esseh12 <Gbenmoese788@gmail.com>
2023-07-06 14:04:40 +0000

    division

---
+++ unsignedint/2-subtraction.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - random subtraction
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	unsigned int num = 200;
+	unsigned int max = 100;
+	unsigned result = num - max;
+
+	printf("Results;%d\n", result);
+
+	return (0);
+}
---
+++ unsignedint/3-multiplication.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Random multiplication
+ *
+ * Return: Alwys 0 (success)
+ */
+int main(void)
+{
+	unsigned int num = 100;
+	unsigned int max = 100;
+	unsigned int result = num * max;
+
+	printf("Result:%d\n",  result);
+
+	return (0);
+}
