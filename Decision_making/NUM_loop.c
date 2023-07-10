a492db1a0f5839e6fc82ad18b07a94762f37a43e
Esseh12 <Gbenmoese788@gmail.com>
2023-07-10 16:54:01 +0000

    NUM_loop.c

---
+++ Decision_making/NUM_loop.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ *
+ */
+int main()
+{
+	/* using loop to print numbers */
+
+	for (int i = 1; i <= 5; ++i)
+	{
+		printf("%d\n", i);
+	}
+       return (0);
+}
