cff5ee85e11da2d82809f3a9d38a503483441c50
Esseh12 <Gbenmoese788@gmail.com>
2023-07-11 19:09:02 +0000

    NATURAL_NUMBER-LOOP.c

---
+++ DO-IT_YOURSELF/FOR-LOOPS.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point of program
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	for (int i = 0; i <= 5; ++i)
+	{
+		printf("Hello, world\n");
+	}
+	return (0);
+}
---
+++ DO-IT_YOURSELF/NATURAL_NUMBER-LOOP.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Result: Always 0 (success)
+ *
+ */
+
+int main(void)
+{
+	for (int i = 0; i <= 100; ++i)
+	{
+		printf("%d\n", i);
+	}
+	return (0);
+}
